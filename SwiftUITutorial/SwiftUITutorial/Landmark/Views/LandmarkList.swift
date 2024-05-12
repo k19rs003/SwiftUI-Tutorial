f3f08afe4ff5fd69d16904638df223764b9d05fe
Moe Abe <71622151+k19rs003@users.noreply.github.com>
2024-05-12 16:45:40 +0900

    feat: LandmarkListにお気に入りフィルターを追加

--- SwiftUITutorial/SwiftUITutorial/Landmark/Views/LandmarkList.swift
+++ SwiftUITutorial/SwiftUITutorial/Landmark/Views/LandmarkList.swift
@@ -8,22 +8,41 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    @State private var showFavoriteOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        landmarks.filter { landmark in
+            (!showFavoriteOnly || landmark.isFavorite)
+        }
+    }
+    
     var body: some View {
-//        List {
-//            LandmarkRow(landmark: landmarks[0])
-//            LandmarkRow(landmark: landmarks[1])
-//        }
         NavigationSplitView {
-            List(landmarks) { landmark in
-                NavigationLink {
-                    LandmarkDetail(landmark: landmark)
-                } label: {
-                    LandmarkRow(landmark: landmark)
-//                        .listRowBackground(Color.clear)
+//            List(filteredLandmarks) { landmark in
+//                NavigationLink {
+//                    LandmarkDetail(landmark: landmark)
+//                } label: {
+//                    LandmarkRow(landmark: landmark)
+////                        .listRowBackground(Color.clear)
+//                }
+//            }
+            List {
+                Toggle(isOn: $showFavoriteOnly) {
+                    Text("Favorites only")
+                }
+                
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+    //                        .listRowBackground(Color.clear)
+                    }
                 }
             }
+            .animation(.default, value: filteredLandmarks)
             .navigationTitle("Landmarks")
-            .scrollContentBackground(.hidden)
+//            .scrollContentBackground(.hidden)
             .padding(.bottom)
         } detail: {
             Text("Select a Landmark")
