1585605889510333343be4b40cc94a3e4585ab3f
Moe Abe <71622151+k19rs003@users.noreply.github.com>
2024-06-13 16:50:21 +0900

    feat: LandmarkDetailにお気に入りボタンを追加

--- SwiftUITutorial/SwiftUITutorial/Landmark/Views/Landmarks/LandmarkDetail.swift
+++ SwiftUITutorial/SwiftUITutorial/Landmark/Views/Landmarks/LandmarkDetail.swift
@@ -8,9 +8,17 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    @Environment(ModelData.self) var modelData
     var landmark: Landmark
     
+    var landmarkIndex: Int {
+        modelData.landmarks.firstIndex(where: { $0.id == landmark.id })!
+    }
+    
     var body: some View {
+        // @Bindableで，@Enviroment(@Observable宣言してる)で持ってきたmodelDataプロパティの双方向データバインディングをサポート 34行目へ！！！
+        @Bindable var modelData = modelData
+        
         ScrollView {
             MapView(coordinate: landmark.locationCoordinate)
                 .frame(height: 300)
@@ -20,8 +28,13 @@ struct LandmarkDetail: View {
                 .padding(.bottom, -130)
             
             VStack(alignment: .leading) {
-                Text(landmark.name)
-                    .font(.title)
+                HStack {
+                    Text(landmark.name)
+                        .font(.title)
+                    // isSetは@Binding．だからFavoriteButtonを変えたら，landmarks.isFavoriteが書き換わる
+                    FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                }
+                
                 HStack {
                     Text(landmark.park)
                         .font(.subheadline)
@@ -47,5 +60,8 @@ struct LandmarkDetail: View {
 }
 
 #Preview {
-    LandmarkDetail(landmark: ModelData().landmarks[0])
+//    LandmarkDetail(landmark: ModelData().landmarks[0])
+    let modelData = ModelData()
+    return LandmarkDetail(landmark: modelData.landmarks[0])
+        .environment(modelData)
 }
